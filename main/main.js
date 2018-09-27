4060552cdb90c7bb3c0d9e2a884cfcdf537043a2
VV4yne <iehouweining@gmail.com>
2018-09-27 11:04:27 +0800

    git push

--- main/main.js
+++ main/main.js
@@ -1,3 +1,8 @@
-module.exports = function main() {
+module.exports = function main(input) {
   // Write your code here
+  var sum = 0;
+  for (var i = 0;i<input.length;i++){
+	  sum = sum + parseInt(input[i]);
+  }
+  return sum;
 };
